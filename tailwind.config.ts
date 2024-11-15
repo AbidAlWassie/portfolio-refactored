92e2d0634e1457ef59926dc7375e9a879381a3a0
Abid Al Wassie <abidalwassie@gmail.com>
2024-11-15 14:51:11 +0600

    refactored project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,10 @@
 module.exports = {
   mode: 'jit',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
